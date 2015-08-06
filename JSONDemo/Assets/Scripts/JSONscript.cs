bab690e910da7d4fcc1f3142fe50b1939544b567
Oervald <neo24dk@gmail.com>
2015-08-06 12:33:53 +0200

    Script for TestScene added

---
+++ JSONDemo/Assets/Scripts/JSONscript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class JSONscript : MonoBehaviour {
+
+	public Text text;
+ 	
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+
+		Debug.Log ("Hello World");
+		WWW json = new WWW ("http://jsonplaceholder.typicode.com/posts");
+		yield return json;
+
+
+		var N = JSON.Parse(json.text);
+		
+
+		
+		System.IO.BinaryReader reader = new System.IO.BinaryReader (json);
+	Object[] jsonObject = JSONNode.Deserialize(reader);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+[System.Serializable]
+public class Query {
+
+	int userid;
+	int id;
+	string title;
+	string body;
+
+	public Query(){
+	}
+	public Query(int _userid, int _id, string _title, string _body){
+		this.userid = _userid;
+		this.id = _id;
+		this.title = _title;
+		this.body = _body;
+	}
+
+
+}
+
---
+++ MentorDanmark/Assets/Scripts/BehaviorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
