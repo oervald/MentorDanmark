34601fea03ed9235fa693d2e2b014410878bef8b
Oervald <neo24dk@gmail.com>
2015-08-07 14:16:01 +0200

    Settings updated

---
+++ MentorDanmark/Assets/Scripts/TestStartBehaviorScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TestStartBehaviorScript : MonoBehaviour {
+
+
+	GameObject startPanel;
+	Re
+
+
+	// Use this for initialization
+	void Start () {
+		startPanel = GameObject.Find("IntroSide");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartTest(){
+	
+		//startPanel.SetActive (false);
+		startPanel.gameObject.SetActive (false);
+		Debug.Log ("Hello World!!");
+
+
+
+
+	}
+}
