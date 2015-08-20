71b10dda40ab1a866cffde6fbcb88e3f14e19807
wuzzqir <thunder0102@hotmail.com>
2015-08-20 15:37:42 +0200

    new art added, mentor site updated, ListItemStudent.cs made, but not finished.

--- MentorDanmark/Assets/Scripts/ListItem/ListItemStudent.cs
+++ MentorDanmark/Assets/Scripts/ListItem/ListItemStudent.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ListItemStudent : ListItem {
 
+	[Header("Student")]
+	public GameObject viewPanel;
+
 	// Use this for initialization
 	void Start () {
-		base.Start ();
+		base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		base.Update ();
+	}
+	
+
+	public void HeadlineClicked ()
+	{
+		if (viewPanel.activeSelf)
+			Fold ();
+		else
+			print (LayoutUtility.GetPreferredHeight (GetComponent<RectTransform> ()));
+		
 	}
 }
